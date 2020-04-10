c7134fcfde3df1a54d6149389fc3fb8e30d3acfe
Gary Rivera <garyrivera@hotmail.com>
2020-04-10 13:09:28 -0400

    Ajout des requetes et rapport pour Q8

---
+++ Q3/CommitChangement.ps1
@@ -0,0 +1,44 @@
+﻿
+    # git log --pretty='commit:%H  ad:%cd cd:%cd' --date=short --dirstat=files,10 > dirstat2.txt
+
+    $commit = ""
+    $dateAut   = ""
+    $matcher=""
+    $dateCommit   = ""
+    $autorEmail= ""
+    $commiterEmail= ""
+
+    foreach($line in Get-Content C:\Users\Gary\Documents\UQAM\H20-MGL-7460\ProjetInd\mgl7460_trav_ind\Q3\dirstat2Test.txt) {
+    
+    
+            if(([regex]::IsMatch($line,"\bcommit:(?<commit>.*)\sad:(?<date>\d\d\d\d-\d\d-\d\d)\scd:(?<dateCommit>\d\d\d\d-\d\d-\d\d)\saE:(?<autorEmail>([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}))\sce:(?<commiterEmail>([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}))$") ) )
+            {  
+
+                $matcher =[regex]::Matches($line,"\bcommit:(?<commit>.*)\sad:(?<date>\d\d\d\d-\d\d-\d\d)\scd:(?<dateCommit>\d\d\d\d-\d\d-\d\d)\saE:(?<autorEmail>([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}))\sce:(?<commiterEmail>([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}))$")
+                $commit =  $matcher[0].Groups["commit"].value.TrimEnd()
+                $dateAut   =  $matcher[0].Groups["date"].value.TrimEnd()
+				$dateCommit   =  $matcher[0].Groups["dateCommit"].value.TrimEnd()
+                $autorEmail=  $matcher[0].Groups["autorEmail"].value.TrimEnd()
+                $commiterEmail=  $matcher[0].Groups["commiterEmail"].value.TrimEnd()
+           
+
+            }
+            elseif([regex]::IsMatch($line,"\b(?<pourcent>(\d|\d\d)\.(\d|\d\d)\%)\s(?<file>.*)$"))
+            {
+                    
+               $matcher =[regex]::Matches($line,"\b(?<pourcent>(\d|\d\d)\.(\d|\d\d)\%)\s(?<file>.*)$")
+               if($matcher.Success)
+                {
+                   $pourcent =  $matcher[0].Groups["pourcent"].value.TrimEnd()
+                   $file   =    $matcher[0].Groups["file"].value.TrimEnd()
+                  
+                   $new_line = "{0};{1};{2};{3};{4};{5};{6}" -f $commit.Trim(), $dateAut.Trim(), $dateCommit.Trim(), $autorEmail, $commiterEmail, $pourcent.Trim(), $file.Trim()
+                
+                   Write-Output   $new_line
+                 
+                  Add-Content  C:\Users\Gary\Documents\UQAM\H20-MGL-7460\ProjetInd\mgl7460_trav_ind\Q3\dirsta_L2.csv  $new_line
+               }
+            
+            }
+            
+}
---
+++ Q8/CommitChangement.ps1
@@ -0,0 +1,43 @@
+﻿
+    #  git log --pretty='commit:%H date:%cd email:%ce comment:%s' --date=short --numstat --grep='fix' --source > log_commit_files_fixed.txt
+
+
+    $commit = ""
+    $date   = ""
+    $email= ""
+    $comment= ""
+    $matcher=""
+
+    foreach($line in Get-Content C:\Users\Gary\Documents\UQAM\H20-MGL-7460\ProjetInd\mgl7460_trav_ind\Q8\log_commit_files_feat.txt) {
+    
+    
+            if(([regex]::IsMatch($line,"commit:(?<commit>.*)\sdate:(?<date>\d\d\d\d-\d\d-\d\d)\semail:(?<email>([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}))\scomment:(?<comment>.*)$") ) )
+            {
+
+                $matcher =  [regex]::Matches($line,"commit:(?<commit>.*)\sdate:(?<date>\d\d\d\d-\d\d-\d\d)\semail:(?<email>([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}))\scomment:(?<comment>.*)$")
+                $commit  =  $matcher[0].Groups["commit"].value.TrimEnd()
+                $date    =  $matcher[0].Groups["date"].value.TrimEnd()
+				$email   =  $matcher[0].Groups["email"].value.TrimEnd()
+                $comment  =  $matcher[0].Groups["comment"].value.TrimEnd()
+           
+            }
+            elseif([regex]::IsMatch($line,"^(?<added>\d*)\s*(?<deleted>\d*)\s(?<file>\S*)$"))
+            {
+                    
+               $matcher =[regex]::Matches($line,"^(?<added>\d*)\s*(?<deleted>\d*)\s(?<file>\S*)$")
+               if($matcher.Success)
+                {
+                   $added =  $matcher[0].Groups["added"].value.TrimEnd()
+                   $deleted  =    $matcher[0].Groups["deleted"].value.TrimEnd()
+                   $file=    $matcher[0].Groups["file"].value.TrimEnd()
+
+                   $new_line = "{0};{1};{2};{3};{4};{5};{6}" -f $commit.Trim(), $date.Trim(), $email,  $added.Trim(),$deleted , $file.Trim(),$comment
+                
+                   Write-Output   $new_line
+                 
+                  Add-Content  C:\Users\Gary\Documents\UQAM\H20-MGL-7460\ProjetInd\mgl7460_trav_ind\Q8\files_feat.csv  $new_line
+               }
+            
+            }
+            
+}
