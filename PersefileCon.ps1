bb46925598a11f40504754c002ccef743483e6bc
garyrivera <garyrivera@hotmail.com>
2021-03-22 21:02:18 -0400

    PerseTXT

---
+++ PersefileCon.ps1
@@ -0,0 +1,32 @@
+﻿
+   $files = Get-ChildItem *.txt
+   $pattern = "^\d{0,3}\,?\d{0,3}\,?\d{0,3}\,?\d{0,3}\,?\d{0,3}\,?d{0,3}[\S|\s]"
+
+        Write-Output   $files
+  
+  ForEach ($file in $files)  {
+
+        
+        $file_new = "NEW_{0}"  -f $file.Name
+        $file_new = $file_new.Replace('.txt','.csv')
+
+
+        Write-Output   $file_new 
+        Write-Output   $file
+
+
+        foreach($line in Get-Content $file) {
+    
+                 if(([regex]::IsMatch($line,$pattern) ) )
+                {  
+
+                    $new_line = $line.Replace(',','')
+                    $new_line = $new_line.Replace(' ** ',' ')
+                    $new_line = $new_line.Replace('    *--------* ',' ')
+
+                }
+
+                Write-Output   $new_line
+                Add-Content   $file_new  $new_line
+        }
+    }
